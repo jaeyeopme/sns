625c4732b618dec4627bee8754aa95d235be6639
Jaeyeop <jaeyeop.dev@gmail.com>
2022-06-15 15:56:30 +0900

    refactor: Validation 이용 Name 유효성 검사 변경 test: Name 테스트 수정

--- src/main/java/me/jaeyeopme/sns/user/domain/Name.java
+++ src/main/java/me/jaeyeopme/sns/user/domain/Name.java
@@ -2,33 +2,26 @@ package me.jaeyeopme.sns.user.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import me.jaeyeopme.sns.common.exception.InvalidArgumentException;
-import org.springframework.util.StringUtils;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class Name {
 
+    @Size(max = 20)
+    @NotBlank
     @Column(name = "name", nullable = false, unique = true)
     private String value;
 
-    private Name(final String value) {
-        verify(value);
-        this.value = value;
-    }
-
     public static Name of(final String value) {
         return new Name(value);
-    }
-
-    private void verify(final String value) {
-        if (!StringUtils.hasText(value) || value.length() > 20) {
-            throw new InvalidArgumentException();
-        }
     }
 
 }
